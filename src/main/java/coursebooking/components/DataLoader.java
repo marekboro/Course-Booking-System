dfbf85e457803a8421a272830916e331557476ce
marekboro <marek.borowicz83@gmail.com>
2020-10-08 14:08:34 +0100

    DataLoader give customers. Test added to verify customers added. also verified via H2.

--- src/main/java/coursebooking/components/DataLoader.java
+++ src/main/java/coursebooking/components/DataLoader.java
@@ -1,4 +1,46 @@
 package coursebooking.components;
 
-public class DataLoader {
+import coursebooking.models.Course;
+import coursebooking.models.Customer;
+import coursebooking.repositories.BookingRepository;
+import coursebooking.repositories.CourseRepository;
+import coursebooking.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    BookingRepository bookingRepository;
+
+    public DataLoader(){
+
+    }
+
+    public void run(ApplicationArguments args){
+        Customer customer1 = new Customer("Ally","Dundee", 24);
+        Customer customer2 = new Customer("Jarrod","Edinburgh", 48);
+        Customer customer3 = new Customer("Steve","Glasgow", 104);
+        Customer customer4 = new Customer("Katie","Stirling", 19);
+        Customer customer5 = new Customer("Anna","Edinburgh", 23);
+        Customer customer6 = new Customer("Jennifer","Glasgow", 22);
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
+        customerRepository.save(customer3);
+        customerRepository.save(customer4);
+        customerRepository.save(customer5);
+        customerRepository.save(customer6);
+
+
+    }
+
 }
--- src/test/java/coursebooking/CourseBookingServiceApplicationTests.java
+++ src/test/java/coursebooking/CourseBookingServiceApplicationTests.java
@@ -1,14 +1,39 @@
 package coursebooking;
 
+import coursebooking.models.Customer;
+import coursebooking.repositories.BookingRepository;
+import coursebooking.repositories.CourseRepository;
+import coursebooking.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CourseBookingServiceApplicationTests {
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	BookingRepository bookingRepository;
+
 
 	@Test
 	void contextLoads() {
 	}
 
+
+	@Test
+	public void hasCustomers(){
+		List<Customer> found = customerRepository.findAll();
+		assertEquals(6,found.size());
+	}
 
 }
