995199d28fc851d3c312d6d709bb5d034c7b29f6
marekboro <marek.borowicz83@gmail.com>
2020-10-08 13:56:58 +0100

    controllers give RESTful routes

--- src/main/java/coursebooking/controllers/CourseController.java
+++ src/main/java/coursebooking/controllers/CourseController.java
@@ -1,9 +1,16 @@
 package coursebooking.controllers;
 
 
+import coursebooking.models.Booking;
+import coursebooking.models.Course;
 import coursebooking.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CourseController {
@@ -11,4 +18,37 @@ public class CourseController {
 
     @Autowired
     CourseRepository courseRepository;
+
+    @GetMapping(value = "/courses")
+    public ResponseEntity<List<Course>> getAllCourses() {
+        return new ResponseEntity<>(courseRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/courses/{id}")
+    public ResponseEntity getCourse(@PathVariable Long id) {
+        return new ResponseEntity<>(courseRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/courses")
+    public ResponseEntity<Course> postCourse(@RequestBody Course course) {
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/courses/{id}")
+    public ResponseEntity<Object> updateCourse(@RequestBody Course course, @PathVariable Long id) {
+        Optional<Course> courseOptional = courseRepository.findById(id);
+        if (!courseOptional.isPresent())
+            return new ResponseEntity<>(course, HttpStatus.NOT_FOUND);
+        course.setId(id);
+        courseRepository.save(course);
+        return new ResponseEntity<>(course, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/courses/{id}")
+    public void deleteCourse(@PathVariable Long id) {
+        courseRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/coursebooking/controllers/CustomerController.java
+++ src/main/java/coursebooking/controllers/CustomerController.java
@@ -1,12 +1,53 @@
 package coursebooking.controllers;
 
+import coursebooking.models.Booking;
+import coursebooking.models.Customer;
 import coursebooking.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CustomerController {
 
     @Autowired
     CustomerRepository customerRepository;
+
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        return new ResponseEntity<>(customerRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity getCustomer(@PathVariable Long id) {
+        return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer) {
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/customers/{id}")
+    public ResponseEntity<Object> updateCustomer(@RequestBody Customer customer, @PathVariable Long id) {
+        Optional<Customer> customerOptional = customerRepository.findById(id);
+        if (!customerOptional.isPresent())
+            return new ResponseEntity<>(customer, HttpStatus.NOT_FOUND);
+        customer.setId(id);
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerRepository.deleteById(id);
+    }
+
+
 }
