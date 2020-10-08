03e8c992e509380ef7da752f762a032f75ae05db
marekboro <marek.borowicz83@gmail.com>
2020-10-08 11:53:09 +0100

    basic setup via Spring initializer : Spring Web, H2, Spring Data JPA, Spring Boot DevTools.

---
+++ src/test/java/coursebooking/CourseBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package coursebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
